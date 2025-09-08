d0336dc95552e78141bcaf8190e3d693e4d40f32
AnweshaSathua <119332714+AnweshaSathua@users.noreply.github.com>
2025-09-08 13:00:22 +0530

    Update task-form.component.ts

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -125,7 +125,7 @@ export class TaskFormComponent implements OnInit {
 
   /** Fetch employee details from backend */
   loadEmployeeDetails(employeeId: string): void {
-    this.http.get<any>(`http:/192.168.0.22:8020/employee/api/employees/${employeeId}`)
+    this.http.get<any>(`http:/192.168.0.22:8243/employee/api/employees/${employeeId}`)
       .subscribe({
         next: (res) => {
           this.employeeName = res.employeeName; // backend should return employeeName
@@ -151,7 +151,7 @@ export class TaskFormComponent implements OnInit {
 
     console.log('Final Payload:', payload);
 
-    this.http.post('http://192.168.0.22:8020/employee/api/v1/tasks/submit', payload)
+    this.http.post('http://192.168.0.22:8243/employee/api/v1/tasks/submit', payload)
       .subscribe({
         next: () => {
           alert('Task saved successfully!');
@@ -162,3 +162,4 @@ export class TaskFormComponent implements OnInit {
       });
   }
 }
+
